6e6db3e2b0e77dcd8dd17d6cbcf3e0a3352d2742
sgreloch <samuelgreloch@yahoo.com>
2024-12-04 13:44:54 -0500

    12

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
@@ -2,6 +2,10 @@ package io.zipcoder.currencyconverterapplication;
 
 public class CurrencyConverter {
     public static Double convert(Double amountOfBaseCurrency, ConvertableCurrency sourceCurrencyType, CurrencyType targetCurrencyType) {
-        return sourceCurrencyType.convert(targetCurrencyType) * amountOfBaseCurrency;
+
+        double sourceCurrencyRate = sourceCurrencyType.convert(targetCurrencyType);
+
+
+        return sourceCurrencyRate * amountOfBaseCurrency;
     }
 }
--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
@@ -1,5 +1,7 @@
 package io.zipcoder.currencyconverterapplication;
 
+import io.zipcoder.currencyconverterapplication.currencies.*;
+
 public enum CurrencyType {
     AUSTRALIAN_DOLLAR(2.70),
     CANADIAN_DOLLAR(2.64),
@@ -25,6 +27,34 @@ public enum CurrencyType {
     }
 
     public static CurrencyType getTypeOfCurrency(ConvertableCurrency currency) {
-        return null;
+        if (currency instanceof AustralianDollar){
+            return AUSTRALIAN_DOLLAR;
+        } else if (currency instanceof Euro){
+            return EURO;
+        }else if (currency instanceof Ringgit){
+            return RINGGIT;
+        }
+        else if (currency instanceof CanadianDollar) {
+            return CANADIAN_DOLLAR;
+        } else if (currency instanceof ChineseYR){
+            return CHINESE_YR;
+        } else if (currency instanceof Franc){
+            return FRANC;
+        }else if(currency instanceof Pound){
+            return POUND;
+        } else if (currency instanceof Rupee){
+            return RUPEE;
+        } else if (currency instanceof SingaporeDollar){
+            return SINGAPORE_DOLLAR;
+        }else if (currency instanceof UniversalCurrency){
+            return UNIVERSAL_CURRENCY;
+        } else if (currency instanceof USDollar){
+            return  US_DOLLAR;
+        }else if (currency instanceof Yen){
+            return YEN;
+        } else {
+
+            return null;
+        }
     }
 }
